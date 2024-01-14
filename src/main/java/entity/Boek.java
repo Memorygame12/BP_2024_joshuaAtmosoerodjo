540b033a635e29fd70eec8b2ed8eaa7dd9636183
Hendrik pawiro <hendrikpawiro2@gmail.com>
2024-01-14 00:48:29 -0300

    add aantal

--- src/main/java/entity/Boek.java
+++ src/main/java/entity/Boek.java
@@ -12,10 +12,11 @@ public class Boek {
         // Standaard constructor
     }
 
-    public Boek(String titel, String auteur, String genre) {
+    public Boek(String titel, String auteur, String genre, int aantal) {
         this.titel = titel;
         this.auteur = auteur;
         this.genre = genre;
+        this.aantal = aantal;
     }
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
@@ -30,6 +31,9 @@ public class Boek {
     @Column(name = "genre")
     private String genre;
 
+    @Column(name = "aantal")
+    private int aantal;
+
     @ManyToMany(mappedBy = "boeken")
     private Set<Categorie> categorieen;
 
@@ -43,4 +47,11 @@ public class Boek {
     public void setGenre(String genre) { this.genre = genre; }
     public Set<Categorie> getCategorieen() { return categorieen; }
     public void setCategorieen(Set<Categorie> categorieen) { this.categorieen = categorieen; }
+    public int getAantal() {
+        return aantal;
+    }
+
+    public void setAantal(int aantal) {
+        this.aantal = aantal;
+    }
 }
